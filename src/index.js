2c9835a5ccf345ce19b0a3490ff462a07d7ddba7
KurczakKING <monikabflaga@gmail.com>
2022-03-06 18:50:46 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
 import { style } from "./css/index.scss";
 
-let heading = document.querySelector("#demo"),
-  sumValue = sum(10, 20);
-
-heading.innerHTML = `30 + 20 = ${sumValue}`;
-
-import Icon from "./assets/img/coffee.jpg";
-
-let myIcon = new Image();
-myIcon.src = Icon;
-document.querySelector("div").appendChild(myIcon);
-document.querySelector("div").classList.add("change");
+let heading = document.querySelector("#demo")
